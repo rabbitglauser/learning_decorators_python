a1c585708ed321794143e8c171150796bee89c2a
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-07-15 15:02:56 +0200

    Add function timing decorator
    A decorator named `get_time` was added to the file. This decorator is used to measure the execution time of any function it wraps, providing a simple utility for performance testing. It features correct rounding, return, and printing functionalities.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from functools import wraps
-from time import perf_counter , sleep
+from time import perf_counter, sleep
 
 
+def get_time(func):
+    """times any function"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = perf_counter()
+        result = func(*args, **kwargs)  # Store function result
+        end_time = perf_counter()
+        total_time = round(end_time - start_time, 2)  # Correct round application
+        print('Time', total_time, 'seconds')
+        return result  # Return function result
+
+    return wrapper  # Correct return position
+
+
+@get_time
 def do_something(param: str):
     """Does something important"""
-
     sleep(1)
     print(param)
 
